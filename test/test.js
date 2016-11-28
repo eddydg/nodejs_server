c3dc7d1ff8aea77e9ffc33007710aa5313d1bf70
eddydg <diablo22007@gmail.com>
2016-11-28 15:07:54 +0100

    Added tests

---
+++ test/test.js
@@ -0,0 +1,28 @@
+var assert = require("assert")
+var todo_utils = require('../todo_utils');
+
+
+
+describe('#Error : Todo is null', function () {
+   it('todo_utils._getAll should throw an error : todo is null', function () {
+      todo_utils._init();
+      assert.throws(function() {
+         todo_utils._getAll((todoList) => { console.log(todoList); })
+      }, /todo is null/);
+    });
+
+});
+
+describe('#Get', function () {
+    it('todo_utils._get(1) should return : A good thing', function () {
+        todo_utils._init();
+        todo_utils._new();
+        todo_utils._add('A thing', (todoList) => {});
+        todo_utils._add('A good thing', (todoList) => {});
+        todo_utils._add('Another thing', (todoList) => {});
+
+        todo_utils._get(1, (thing) => {
+            assert.equal(thing, 'A good thing');
+        });
+    });
+});
